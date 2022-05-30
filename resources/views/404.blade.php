00070b7ba5b1dcdcc965162a299b874d63ce2bc0
MurilloDiana <dianamurillo.467@gmail.com>
2022-05-30 15:13:29 -0400

    404
    modificacion

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -1,7 +1,6 @@
-@extends('layouts.master')
-@section('content')
+
     <div >
         <h2 style="text-align: center"> Under Construction </h2>
-        <img src="{{asset('img/404.png')}}" alt=""  style="width:350px height:350px">
+
+        <div aling="top"><img src="{{asset('img/404.png')}}" alt=""  style="width:350px height:350px"></div>
     </div>
-@endsection
