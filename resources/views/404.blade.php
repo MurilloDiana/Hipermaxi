ccd91a969ec976bef54a7d0f8ef37c3b6239442c
renypapuh <51518380+renypapuh@users.noreply.github.com>
2022-05-31 23:53:08 -0400

    modificacion
    prueba de modificacion

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -2,5 +2,5 @@
     <div >
         <h2 style="text-align: center"> Under Construction </h2>
 
-        <div aling="top"><img src="{{asset('img/404.png')}}" alt=""  style="width:350px height:350px"></div>
+        <div ><img src="{{asset('img/404.png')}}" alt=""  style="width:350px height:350px"></div>
     </div>
