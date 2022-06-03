9139c6866703e4918ab6ac0b29579bde4f50db39
MauricioD98 <101749218+MauricioD98@users.noreply.github.com>
2022-06-03 15:13:53 +0100

    Prueba
    XD

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -2,7 +2,7 @@
 @section('title', '404')
 @section('content')
     <div >
-        <h2 style="text-align: center"> Under Construction </h2>
+        <h2 style="text-align: center"> XD </h2>
         <img src="{{asset('img/404.png')}}" alt=""  style="width:350px height:350px">
     </div>
 @endsection
