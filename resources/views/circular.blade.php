d8f9e082e71f91015424d1ae22fde3a8bc35ab8d
MurilloDiana <dianamurillo.467@gmail.com>
2022-06-04 23:27:50 -0400

    grafica
    circular corregida

--- resources/views/circular.blade.php
+++ resources/views/circular.blade.php
@@ -4,7 +4,9 @@
 <script src="https://code.highcharts.com/modules/exporting.js"></script>
 <script src="https://code.highcharts.com/modules/export-data.js"></script>
 <script src="https://code.highcharts.com/modules/accessibility.js"></script>
+<figure class="highcharts-figure">
 <a class="btn btn-primary" href="{{ url('chart') }}" role="button">Gráfico de Líneas</a>
+<div id="container"></div>
 <script> 
 Highcharts.chart('container', {
     chart: {
@@ -14,7 +16,7 @@ Highcharts.chart('container', {
         type: 'pie'
     },
     title: {
-        text: 'Grafica Circular'
+        text: 'Browser market shares in January, 2018'
     },
     tooltip: {
         pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
@@ -37,9 +39,38 @@ Highcharts.chart('container', {
     series: [{
         name: 'Brands',
         colorByPoint: true,
-        datas:  <?php echo json_encode($datas) ?>;
+        data: [{
+            name: 'Chrome',
+            y: 61.41,
+            sliced: true,
+            selected: true
+        }, {
+            name: 'Internet Explorer',
+            y: 11.84
+        }, {
+            name: 'Firefox',
+            y: 10.85
+        }, {
+            name: 'Edge',
+            y: 4.67
+        }, {
+            name: 'Safari',
+            y: 4.18
+        }, {
+            name: 'Sogou Explorer',
+            y: 1.64
+        }, {
+            name: 'Opera',
+            y: 1.6
+        }, {
+            name: 'QQ',
+            y: 1.2
+        }, {
+            name: 'Other',
+            y: 2.61
+        }]
     }]
 });
 </script>
-
+</figure>
 @endsection
