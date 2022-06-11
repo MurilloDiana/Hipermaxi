af06bd19b244d16f24ae21e7c4bfac58240a0a09
MurilloDiana <dianamurillo.467@gmail.com>
2022-06-11 19:06:06 -0400

    prueba
    prueba

--- database/migrations/2022_06_08_205206_add_foreign_key.php
+++ database/migrations/2022_06_08_205206_add_foreign_key.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->id()->unique();
             $table->foreign('id')->references('CODIGO')->on('empleados');
-        });
+        }); 
     }
 
     /**
