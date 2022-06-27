42d5df3937e1d854f9a6dbb1d382189ce02781db
bikejojo <87353248+bikejojo@users.noreply.github.com>
2022-06-26 20:50:16 -0400

    BASEda
    baseDatosPermiso

---
+++ app/Models/permiso.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class permiso extends Model
+{ 
+   // use HasFactory;
+   protected $primaryKey = "id";
+   protected $fillable = [      
+       'FECHA_INI',
+       'FECHA_FIN',
+       'ASUNTO',
+       'DIAS'
+   ];
+}
---
+++ database/migrations/2022_06_27_004230_create_permisos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permisos', function (Blueprint $table) {
+            $table->id();
+            $table->date('FECHA_INI');
+            $table->date('FECHA_FIN');
+            $table->STRING('ASUNTO');
+            $table->unsignedBigInteger('DIAS');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('permisos');
+    }
+};
